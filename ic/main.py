d7fa0be1b7ca13e5abac6b2499238e4ecef44404
Gaby <mgmendoza@alumni.usc.edu>
2024-04-01 16:23:27 -0700

    writing function to create the scn file

--- ic/main.py
+++ ic/main.py
@@ -6,7 +6,7 @@ import argparse
 import json
 import sys
 from pathlib import Path
-import time
+import os
 
 # Add the bluesky package to the path
 top_level_path = Path(__file__).resolve().parent.parent
@@ -14,13 +14,21 @@ print(str(top_level_path))
 sys.path.append(str(top_level_path))
 import bluesky as bs
 
+
+# Bluesky settings
+T_STEP = 10000
+MANEUVER = True
+GUI = False
+LOG = True
+SIMDT = 1
+
 parser = argparse.ArgumentParser(description='Process a true/false argument.')
 parser.add_argument('--gui', action='store_true', help='Flag for running with gui.')
 parser.add_argument('--file', type=str, required=True, help='The path to the test case json file.')
 args = parser.parse_args()
 
 
-def run_from_json(file = None, run_gui = False):
+def run_from_json(file = None):
     """
     Run the bluesky simulation from a JSON file.
     """
@@ -32,34 +40,34 @@ def run_from_json(file = None, run_gui = False):
     with open(file) as f:
         data = json.load(f)
         print(f"Opened file {file}")
+    return data
 
+
+def create_scenario(data, scene_path, scene_name, run_gui = False):
     # Create the BlueSky simulation
     if not run_gui:
         bs.init(mode="sim", detached=True)
     else:
         bs.init(mode="sim")
         bs.net.connect()
-    
-    return data
 
-
-def create_uav(data):
+    stack_commands = [] 
+    init_settings = "00:00:00.00>TRAILS ON\n00:00:00.00>PAN OAK\n"
+    stack_commands.append(init_settings)
     flights = data['flights'] # Let's get a name to refer to these vehicles
     vertiports = data['vertiports']
-    stack_commands = []
+
+    # if LOG:
+    #     bs.stack.stack(f"CRELOG rb 1")
+    #     bs.stack.stack(f"rb  ADD id, lat, lon, alt, tas, vs, hdg")
+    #     bs.stack.stack(f"rb  ON 1  ")
+
     for flight in flights:
         aircraft_id = flight['aircraft_id']
         origin_vertiport_id = flight['origin_vertiport_id']
         origin_vertiport = vertiports[origin_vertiport_id]
         appearance_time = flight['appearance_time']
 
-    
-        bs.stack.stack("PAN OAK")
-        SCN_PATH ='/home/gaby/Documents/UCB/AAM/GIT/bluesky-IC/scenario'
-        SCN_NAME = 'TEST_IC'
-        bs.stack.stack(f"IC {SCN_PATH}/{SCN_NAME}")
-        bs.stack.stack("DT 1; FF")
-        # Generate stack commands for each request
         for request in flight['requests']:
             destination_vertiport_id = request['destination_vertiport_id']
             destination_vertiport = vertiports[destination_vertiport_id]
@@ -67,7 +75,6 @@ def create_uav(data):
             origin_vertiport_id = request['destination_vertiport_id']
             origin_vertiport = vertiports[origin_vertiport_id]
 
-            # Calculate values for stack command
             or_lat = origin_vertiport['latitude']
             or_lon = origin_vertiport['longitude']
             des_lat = destination_vertiport['latitude']
@@ -76,19 +83,44 @@ def create_uav(data):
             alt = 'FL250'  # placeholder
             spd = 200  # placeholder
             hdg = 0  # placeholder
-
-            # Generate stack command for destination vertiport
-            # print(f"CRE {aircraft_id}, {type},{lat}, {lon}, {hdg}, {alt}, {spd}\n")
-
-            bs.stack.stack(f"CRE {aircraft_id} {type} {or_lat} {or_lon} {hdg} {alt} {spd}\n")
-            bs.stack.stack(f"ADDWPT {aircraft_id} {des_lat}, {des_lon}")
-            bs.stack.stack("TRAIL ON")
+            time_stamp = '00:00:00.00'
+            stack_commands.append(f"{time_stamp}>CRE {aircraft_id} {type} {or_lat} {or_lon} {hdg} {alt} {spd}\n")
+            stack_commands.append(f"{time_stamp}>DEST {aircraft_id} {des_lat}, {des_lon}\n")
+            # bs.stack.stack(f"CRE {aircraft_id} {type} {or_lat} {or_lon} {hdg} {alt} {spd}\n")
+            # bs.stack.stack(f"DEST {aircraft_id} {des_lat}, {des_lon}")
         
-        bs.stack.stack("OP")
+        write_scenario(scene_path, scene_name, stack_commands)
+        # bs.stack.stack("OP")
 
+    return
+
+def write_scenario(SCN_PATH, SCN_NAME, stack_commands):
+    text = ''.join(stack_commands)
+    
+    # Create directory if it doesn't exist
+    directory = f'{SCN_PATH}/{SCN_NAME}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Write the text to the scenario file
+    with open(f'{directory}/{SCN_NAME}.scn', "w") as file:
+        file.write(text)
+
+              
+
+
+def create_uav(data):
+    flights = data['flights'] # Let's get a name to refer to these vehicles
+    vertiports = data['vertiports']
+    for flight in flights:
+        aircraft_id = flight['aircraft_id']
+        origin_vertiport_id = flight['origin_vertiport_id']
+        origin_vertiport = vertiports[origin_vertiport_id]
+        appearance_time = flight['appearance_time']
+    return 
+
+    
         
-        # time.sleep(10)
-        # bs.stack.stack("QUIT")
 
 
 
@@ -99,10 +131,13 @@ if __name__ == "__main__":
     # python3 main.py --file ./../test_cases/case1.json
     file_name = args.file
     assert Path(file_name).is_file(), f"File {file_name} does not exist."
+    data = run_from_json(file_name)
+    # The two below will become args in bash file
+    SCN_PATH ='/home/gaby/Documents/UCB/AAM/GIT/bluesky-IC/scenario'
+    SCN_NAME = 'TEST_IC'
     if args.gui:
         # run_from_json(file_name, run_gui=True)
         # Always call as false because the gui does not currently work
-        data = run_from_json(file_name, run_gui=False)
+        create_scenario(data, SCN_PATH, SCN_NAME, run_gui=False)
     else:
-        data = run_from_json(file_name)
-        create_uav(data)
+        create_scenario(data, SCN_PATH, SCN_NAME)
