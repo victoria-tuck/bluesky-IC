aa4cfa34f8b05a9f0bb419364ad511570243f2ea
Gaby <mgmendoza@alumni.usc.edu>
2024-05-01 13:34:56 -0700

    commiting changes to merge victoria's changes

---
+++ ic/int_allocation.py
@@ -0,0 +1,4 @@
+import cvxpy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+
--- ic/test_fisher_allocation.py
+++ ic/test_fisher_allocation.py
@@ -3,17 +3,30 @@ import numpy as np
 
 
 def test_update_agents():
+    """
+    Function to update agents parameters
+    num_agents (int): number of agents 
+    num_goods (int): number of goods
+    constraints_per_agent (list, nx1): number of constraints per agent
+    w (list, num_agetns x 1): budget of each agent 
+    p (list, num_goods x 1): price of each good
+    r (list, num_agents x 1): rebates of each agent
+    constraints (list, num_agents x 1): constraints of each agent
+    """
     print("Testing agent update")
     num_agents, num_goods, constraints_per_agent = 5, 10, [2, 3, 4, 5, 6]
     w = np.random.rand(num_agents)*10
     u = np.random.rand(num_agents, num_goods)*5
     p = np.random.rand(num_goods)*10
     r = [np.random.rand(constraints_per_agent[i])*10 for i in range(num_agents)]
+    print('budget:',w,'utility: ', u,'price: ', p,'rebate: ', r)
     constraints = [(np.random.rand(constraints_per_agent[i], num_goods)*10, np.random.rand(constraints_per_agent[i])*10) for i in range(num_agents)]
+    print('constraints: ',constraints)
     y = np.random.rand(num_agents, num_goods)*10
+    print('y:',y)
     beta = 1
     x = update_agents(w, u, p, r, constraints, y, beta)
-    print(x)
+    print("x:",x)
 
 
 def test_update_market():
