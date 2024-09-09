b14047d17635abaf4af7a57a8ff7e223b0c41193
Victoria Tuck <victoria_tuck@berkeley.edu>
2024-09-09 16:19:21 -0700

    Creating more restricted test cases

--- ic/case_random_generator.py
+++ ic/case_random_generator.py
@@ -9,12 +9,13 @@ from math import radians, sin, cos, sqrt, atan2
 
 # Simulation time settings
 START_TIME = 1 # multiple of timestep
-END_TIME = 100
+END_TIME = 90
 TIME_STEP = 1
-AUCTION_DT = 10 # every 15 timesteps there is an auction
+AUCTION_DT = 12 # every 15 timesteps there is an auction
+RUNWAY_PERIOD = 6 # time between takeoff/landing
 
 # Case study settings
-N_FLIGHTS = random.randint(10, 15)
+N_FLIGHTS = 40 # random.randint(10, 15)
 NUM_FLEETS = 10
 
 # change the request 000 for always be 0 - done
@@ -38,6 +39,15 @@ vertiports = {
     "V005": {"latitude": 37.38556649999999, "longitude": -121.9723564, "landing_capacity": random.randint(1, 3), "takeoff_capacity": random.randint(1, 5), "hold_capacity": 8},
     "V006": {"latitude": 37.25214395119753, "longitude": -122.4066509403772, "landing_capacity": random.randint(1, 3), "takeoff_capacity": random.randint(1, 5), "hold_capacity": 8},
     "V007": {"latitude": 38.58856301092047, "longitude": -121.5627454937505, "landing_capacity": random.randint(1, 3), "takeoff_capacity": random.randint(1, 5), "hold_capacity": 8},
+}
+vertiports = {
+    "V001": {"latitude": 37.766699, "longitude": -122.3903664, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V002": {"latitude": 37.8361761, "longitude": -122.2668028, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V003": {"latitude": 37.7835538, "longitude": -122.5067642, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V004": {"latitude": 37.9472484, "longitude": -122.4880737, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V005": {"latitude": 37.38556649999999, "longitude": -121.9723564, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V006": {"latitude": 37.25214395119753, "longitude": -122.4066509403772, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
+    "V007": {"latitude": 38.58856301092047, "longitude": -121.5627454937505, "landing_capacity": 1, "takeoff_capacity": 1, "hold_capacity": 8},
 }
 
 # This is for testing of case #3, similarly in generate_routes function set capacity = 0 to test convergence
@@ -70,16 +80,18 @@ def generate_flights():
 
     max_travel_time = route_dict[max(route_dict, key=route_dict.get)]
     last_auction =  END_TIME - max_travel_time - AUCTION_DT
-    auction_intervals = list(range(START_TIME, END_TIME, AUCTION_DT))
+    auction_intervals = list(range(START_TIME - 1, END_TIME, AUCTION_DT))
+    runway_times = list(range(START_TIME, END_TIME, RUNWAY_PERIOD))
 
     for i in range(N_FLIGHTS):  
         flight_id = f"AC{i+1:03d}"
         
         # Select a random auction interval for the appearance time
         auction_interval = random.choice(auction_intervals[:(np.abs(np.array(auction_intervals) - last_auction)).argmin()])
-        appearance_time = random.randint(auction_interval, auction_interval + AUCTION_DT) # to avoid flights appearing after the last auction, this is also constraint by the maximu travel time for node creation
+        appearance_time = random.randint(auction_interval, auction_interval + AUCTION_DT - 1) # to avoid flights appearing after the last auction, this is also constraint by the maximu travel time for node creation
         # appearance_time = random.randint(1, 50) #needs to be changes using end time variable
 
+        # print(f"auction interval: {auction_interval}, appearance time: {appearance_time}")
         # Choose origin vertiport
         origin_vertiport_id = random.choice(allowed_origin_vertiport)
         allowed_origin_vertiport.remove(origin_vertiport_id)
@@ -89,7 +101,8 @@ def generate_flights():
             destination_vertiport_id = random.choice(vertiports_list)
 
         # request_departure_time = appearance_time + random.randint(5, 10)
-        request_departure_time = random.randint(auction_interval + AUCTION_DT, auction_interval + 2*AUCTION_DT)
+        first_valid_departure = math.ceil((auction_interval + AUCTION_DT) / RUNWAY_PERIOD) * RUNWAY_PERIOD
+        request_departure_time = random.choice(list(range(first_valid_departure, first_valid_departure + 10, RUNWAY_PERIOD)))
         # delay = random.randint(1, 5)
         # second_departure_time = request_departure_time + delay
         travel_time = route_dict.get((origin_vertiport_id , destination_vertiport_id), None)
@@ -186,7 +199,7 @@ def generate_routes(vertiports):
                 # this is also somthing that will be moved to each agent's bid
                 distance = calculate_distance(origin_data, destination_data) # km
                 speed = 90 # placeholder, we need to add specific vehicle speed in knots (Wisk)
-                travel_time = math.ceil(distance * 0.5399568 * 60  / speed)   # cover distance from km to naut.miles then hr to min
+                travel_time = math.ceil(math.ceil(distance * 0.5399568 * 60  / speed) / RUNWAY_PERIOD) * RUNWAY_PERIOD  # cover distance from km to naut.miles then hr to min
                 route = {
                     "origin_vertiport_id": origin_id, 
                     "destination_vertiport_id": destination_id, 
