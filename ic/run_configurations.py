90eb818f5f7031a760929ae2b5472dbd8f81f1ec
Gaby <mgmendoza@alumni.usc.edu>
2024-09-16 12:33:34 -0700

    **GOLDEN BRANCH: Uncontested example converging to expected allocations

--- ic/run_configurations.py
+++ ic/run_configurations.py
@@ -3,7 +3,7 @@ import os
 from itertools import product
 
 # Define the parameter values to vary
-BETA_values = [100, 20, 50, 60, 10]
+BETA_values = [10, 20, 50, 60, 100]
 dropout_good_valuation_values = [1]
 default_good_valuation_values = [1]
 price_default_good_values = [10]
