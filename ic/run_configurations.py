3c2d701661b82969b911ede8b5425512256971d2
Victoria Tuck <victoria_tuck@berkeley.edu>
2024-09-16 22:37:58 -0700

    Pulling in contested case (case2.json) from vcg branch

--- ic/run_configurations.py
+++ ic/run_configurations.py
@@ -3,7 +3,8 @@ import os
 from itertools import product
 
 # Define the parameter values to vary
-BETA_values = [10, 20, 50, 60, 100]
+# BETA_values = [10, 20, 50, 60, 100]
+BETA_values = [10, 100]
 dropout_good_valuation_values = [1]
 default_good_valuation_values = [1]
 price_default_good_values = [10]
@@ -18,7 +19,7 @@ main_script_path = os.path.join(os.path.dirname(__file__), 'main.py')
 for idx, (BETA, dropout_good_valuation, default_good_valuation, price_default_good, rebate_frequency) in enumerate(parameter_combinations):
     args = [
         "python", main_script_path,
-        "--file", "test_cases/casef_20240614_153258.json",
+        "--file", "test_cases/case2.json",
         "--method", "fisher",
         "--force_overwrite",
         "--BETA", str(BETA),
